b909cc9fdf04c3cb1f5cc4113b8bacad2d7ba663
aniab <aniab@ANIA>
2024-01-12 15:34:33 +0100

    Adding ApplicationDbContext and DataBaseMigration

---
+++ HabitTracker.DataAccess/ApplicationDbContext.cs
@@ -0,0 +1,25 @@
+﻿
+using HabitTracker.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+using System.Collections.Specialized;
+using System.ComponentModel;
+
+namespace HabitTracker.DataAccess.Data
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options): base(options)
+        {
+
+        }
+
+        public DbSet<Habit> Habits {  get; set; }
+        public DbSet<TimePeriod> TimePeriods { get; set; }
+        public DbSet<HabitRealization> HabitRealizations { get; set; }
+
+
+    }
+
+
+}
---
+++ HabitTracker.DataAccess/Migrations/20240112143320_addingBasicTablesToDb.cs
@@ -0,0 +1,91 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HabitTracker.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class addingBasicTablesToDb : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Habits",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<int>(type: "int", maxLength: 50, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Habits", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "TimePeriods",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    LevelId = table.Column<int>(type: "int", nullable: false),
+                    LevelName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    HabitId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TimePeriods", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_TimePeriods_Habits_HabitId",
+                        column: x => x.HabitId,
+                        principalTable: "Habits",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "HabitRealizations",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    IfExecuted = table.Column<bool>(type: "bit", nullable: false),
+                    DaysRangeId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_HabitRealizations", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_HabitRealizations_TimePeriods_DaysRangeId",
+                        column: x => x.DaysRangeId,
+                        principalTable: "TimePeriods",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_HabitRealizations_DaysRangeId",
+                table: "HabitRealizations",
+                column: "DaysRangeId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TimePeriods_HabitId",
+                table: "TimePeriods",
+                column: "HabitId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "HabitRealizations");
+
+            migrationBuilder.DropTable(
+                name: "TimePeriods");
+
+            migrationBuilder.DropTable(
+                name: "Habits");
+        }
+    }
+}
