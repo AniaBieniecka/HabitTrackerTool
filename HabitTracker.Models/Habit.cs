b0753f9553ce940c7535bc1760b73b19a1fb78da
aniab <aniab@ANIA>
2024-02-02 23:44:00 +0100

    Altering habit model and create view (adding Quantity per week property)

--- HabitTracker.Models/Habit.cs
+++ HabitTracker.Models/Habit.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
+using System.Runtime.ConstrainedExecution;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -13,7 +15,16 @@ namespace HabitTracker.Models
 
         [Required]
         [MaxLength(50)]
-        public string Name { get; set; }
-            
+        public string Name { get; set; }        
+        
+        [DisplayName("How often per week")]
+        [Range(1, 7, ErrorMessage = "Wartość musi być większa lub równa 1, ale mniejsza od 7.")]
+        public int QuantityPerWeek {  get; set; }
+
+
+        public Habit() {
+            QuantityPerWeek = 7;
+        }
+
     }
 }
