5014b5acd66ca1facfd1520f6322d2221d780548
Ania Bieniecka <annabieniecka5@gmail.com>
2024-03-08 23:07:05 +0100

    Added IActionResult UpdateIfExecuted to change habit realization status onclick

--- HabitTrackerWeb/Controllers/HabitRealizationController.cs
+++ HabitTrackerWeb/Controllers/HabitRealizationController.cs
@@ -1,7 +1,9 @@
 ﻿using HabitTracker.DataAccess.Repository.IRepository;
+using HabitTracker.DataAccess.Repositry;
 using HabitTracker.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
 
 namespace HabitTrackerWeb.Controllers
 {
@@ -20,6 +22,27 @@ namespace HabitTrackerWeb.Controllers
             List<Habit> habits= _unitOfWork.Habit.GetAll(includeProperties: "habitRealizations").ToList();
 
             return View(habits);
+        }
+
+        [HttpPost]
+        public IActionResult UpdateIfExecuted(int id)
+        {
+            var item =  _unitOfWork.HabitRealization.Get(u=>u.Id == id);
+            if (item == null)
+            {
+                return NotFound();
+            }
+
+            if (item.IfExecuted == 0)
+            {
+                item.IfExecuted = 1;
+            }
+            else item.IfExecuted = 0;
+
+            _unitOfWork.HabitRealization.Update(item);
+            _unitOfWork.Save();
+
+            return Ok();
         }
     }
 }
