2456f4567372925ba39fd209069b87e7a1484ca5
Ania Bieniecka <annabieniecka5@gmail.com>
2024-03-09 23:45:08 +0100

    Adding another status for property ifExecuted

--- HabitTrackerWeb/Controllers/HabitRealizationController.cs
+++ HabitTrackerWeb/Controllers/HabitRealizationController.cs
@@ -20,7 +20,7 @@ namespace HabitTrackerWeb.Controllers
 
         public IActionResult HabitsWeekly()
         {
-            List<Habit> habits= _unitOfWork.Habit.GetAll(includeProperties: "habitRealizations").ToList();
+            List<Habit> habits = _unitOfWork.Habit.GetAll(includeProperties: "habitRealizations").ToList();
 
             return View(habits);
         }
@@ -30,23 +30,26 @@ namespace HabitTrackerWeb.Controllers
         {
             try
             {
-            var item =  _unitOfWork.HabitRealization.Get(u=>u.Id == id);
-            if (item == null)
-            {
-                return NotFound();
-            }
+                var item = _unitOfWork.HabitRealization.Get(u => u.Id == id);
+                if (item == null)
+                {
+                    return NotFound();
+                }
 
-            if (item.IfExecuted == 0)
-            {
-                item.IfExecuted = 1;
-            }
-            
-            else item.IfExecuted = 0;
+                if (item.IfExecuted == 0)
+                {
+                    item.IfExecuted = 1;
+                }
+                else if (item.IfExecuted == 1)
+                {
+                    item.IfExecuted = 2;
+                }
+                else item.IfExecuted = 0;
 
-            _unitOfWork.HabitRealization.Update(item);
-            _unitOfWork.Save();
+                _unitOfWork.HabitRealization.Update(item);
+                _unitOfWork.Save();
 
-            return Json(new { ifExecuted = item.IfExecuted });
+                return Json(new { ifExecuted = item.IfExecuted });
             }
             catch (Exception ex)
             {
