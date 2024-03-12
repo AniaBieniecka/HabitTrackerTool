97c1609440ea55b9c9bec9f3033e2fedc826cf11
Ania Bieniecka <annabieniecka5@gmail.com>
2024-03-12 23:55:58 +0100

    Added new models ViewSetting, AvailableColor, AvailableIcon

---
+++ HabitTracker.Models/AvailableIcon.cs
@@ -0,0 +1,38 @@
+﻿namespace HabitTracker.Models
+{
+    public class AvailableIcon
+    {
+        public List<string> AvailableIconsDone { get; set; }
+        public List<string> AvailableIconsPartiallyDone { get; set; }
+        public AvailableIcon()
+        {
+            AvailableIconsDone = new List<string>
+            {
+                "bi bi-check-square-fill",
+                "bi bi-star-fill",
+                "bi bi-balloon-heart-fill",
+                "bi bi-check-circle-fill",
+                "bi bi-gift-fill",
+                "bi bi-hand-thumbs-up-fill",
+                "bi bi-emoji-sunglasses-fill",
+                "bi bi-rocket-takeoff-fill"
+
+
+            };
+
+            AvailableIconsPartiallyDone = new List<string>
+            {
+                "bi bi-check-square",
+                "bi bi-star",
+                "bi bi-balloon-heart",
+                "bi bi-check-circle",
+                "bi bi-gift",
+                "bi bi-hand-thumbs-up",
+                "bi bi-emoji-sunglasses",
+                "bi bi-rocket-takeoff"
+
+            };
+        }
+
+    }
+}
---
+++ HabitTracker.Models/ViewSetting.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HabitTracker.Models
+{
+    public class ViewSetting
+    {
+        public int Id { get; set; }
+
+        public string Color { get; set; }
+
+        public string IconPartiallyDone { get; set; }
+        
+
+        [ValidateNever]
+        public string IconDone { get; set; }
+
+        [ValidateNever]
+        [NotMapped]
+        public List<string> availableColors { get; set; }
+        [ValidateNever]
+        [NotMapped]
+        public List<string> availableIconsDone { get; set; }
+        [ValidateNever]
+        [NotMapped]
+        public List<string> availableIconsPartiallyDone { get; set; }
+
+
+    }
+}
