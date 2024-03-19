2f63630eeed5ff5bb74bcf0f6e53fafec6aa723d
Ania Bieniecka <annabieniecka5@gmail.com>
2024-03-19 15:10:29 +0100

    Modified Habit controller and HabitsCurrentWeek view

--- HabitTrackerWeb/Controllers/HabitRealizationController.cs
+++ HabitTrackerWeb/Controllers/HabitRealizationController.cs
@@ -21,7 +21,7 @@ namespace HabitTrackerWeb.Controllers
 
         public IActionResult HabitsWeekly(int? week, int? year)
         {
-            List<Habit> habits = _unitOfWork.Habit.GetAll(u=>u.WeekNumber==week&& u.Year==year, includeProperties: "habitRealizations").ToList();
+            List<Habit> habits = _unitOfWork.Habit.GetAll(u => u.WeekNumber == week && u.Year == year, includeProperties: "habitRealizations").ToList();
 
             foreach (Habit hab in habits)
             {
@@ -34,17 +34,25 @@ namespace HabitTrackerWeb.Controllers
         public IActionResult HabitsCurrentWeek()
         {
 
-            int week = CultureInfo.CurrentCulture.Calendar.GetWeekOfYear(DateTime.Now, CalendarWeekRule.FirstFourDayWeek, DayOfWeek.Monday);
-            int year = DateTime.Now.Year;
+            int weekCurrent = CultureInfo.CurrentCulture.Calendar.GetWeekOfYear(DateTime.Now, CalendarWeekRule.FirstFourDayWeek, DayOfWeek.Monday);
+            DateOnly today = DateOnly.FromDateTime(DateTime.Now);
+            DateTime mondayDate = DateTime.Now.AddDays(-(int)DateTime.Now.DayOfWeek + 1);
+            int year = DateOnly.FromDateTime(mondayDate).Year;
 
-            List<Habit> habits = _unitOfWork.Habit.GetAll(u => u.WeekNumber == week && u.Year == year, includeProperties: "habitRealizations").ToList();
+            List<Habit> habits = _unitOfWork.Habit.GetAll(u => u.WeekNumber == weekCurrent && u.Year == year, includeProperties: "habitRealizations").ToList();
+
+            HabitsCurrentWeekVM habitsCurrentWeekVM = new HabitsCurrentWeekVM();
+           
+            bool habitsHasAnyData = _unitOfWork.Habit.HasAnyData();
+            habitsCurrentWeekVM.habitsHasAnyData = habitsHasAnyData;
+            habitsCurrentWeekVM.habits = habits;
 
             foreach (Habit hab in habits)
             {
                 hab.ViewSetting = _unitOfWork.ViewSetting.Get(u => u.Id == 1);
             }
 
-            return View(habits);
+            return View(habitsCurrentWeekVM);
         }
 
         [HttpGet]
