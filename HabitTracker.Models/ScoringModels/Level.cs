34377b7e876da6029b8e5ce670c77239654dc8e5
Ania Bieniecka <annabieniecka5@gmail.com>
2024-03-24 17:46:14 +0100

    Created models for scoring: Score, Level, LevelList

---
+++ HabitTracker.Models/ScoringModels/Level.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HabitTracker.Models.ScoringModels
+{
+    public class Level
+    {
+        public int Id { get; set; }
+        public int MinimumScore { get; set; }
+
+        public Level(int id, int minimumScore)
+        {
+            Id = id;    
+            MinimumScore = minimumScore;
+        }
+
+    }
+}
---
+++ HabitTracker.Models/ScoringModels/LevelList.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HabitTracker.Models.ScoringModels
+{
+    public class LevelList
+    {
+        public List<Level> Levels { get; set; }
+
+        public LevelList()
+        {
+            Levels = new List<Level>
+        {
+
+            new Level(1, 0),
+            new Level(2, 200),
+            new Level(3, 500),
+            new Level(4, 1000),
+            new Level(5, 1500),
+            new Level(6, 2000),
+            new Level(7, 2500),
+            new Level(8, 3000),
+        };
+        }
+
+    }
+}
