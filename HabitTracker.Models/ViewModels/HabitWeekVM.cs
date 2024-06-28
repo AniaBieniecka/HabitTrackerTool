6f547b7d79a37a95c93dd31008b10f86a255a589
Ania Bieniecka <annabieniecka5@gmail.com>
2024-06-28 23:57:05 +0200

    Changed models and added new migration to DB

---
+++ HabitTracker.Models/ViewModels/HabitWeekVM.cs
@@ -0,0 +1,18 @@
+﻿using HabitTracker.Models.ScoringModels;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HabitTracker.Models.ViewModels
+{
+    public class HabitWeekVM
+    {
+        public HabitWeek habitWeek { get; set; }
+
+        public string habitName { get; set; }
+        public int habitID { get; set; }
+    }
+}
