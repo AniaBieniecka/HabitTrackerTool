00e90ee081d021a210e44bf7b77c8211fe808b08
Ania Bieniecka <annabieniecka5@gmail.com>
2024-07-03 23:52:56 +0200

    Created migration in order to create user tables in DB

--- HabitTracker.DataAccess/Migrations/20240703111850_addedIdentityToDB.Designer.cs
+++ HabitTracker.DataAccess/Migrations/20240703111850_addedIdentityToDB.Designer.cs
@@ -4,6 +4,7 @@ using HabitTracker.DataAccess.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace HabitTracker.DataAccess.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240703111850_addedIdentityToDB")]
+    partial class addedIdentityToDB
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
