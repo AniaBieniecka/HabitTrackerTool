901ddb365f7288c5153a6e3e637d65feeb888d38
Ania Bieniecka <annabieniecka5@gmail.com>
2024-07-03 23:55:00 +0200

    Added configuration of Identity service

--- HabitTrackerWeb/Program.cs
+++ HabitTrackerWeb/Program.cs
@@ -4,6 +4,7 @@ using HabitTracker.DataAccess.Repository.IRepository;
 using HabitTracker.DataAccess.Repositry;
 using HabitTrackerWeb.Controllers.Services;
 using HabitTrackerWeb.Service;
+using Microsoft.AspNetCore.Identity;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,6 +12,9 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
 options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddIdentity<IdentityUser, IdentityRole>()
+    .AddEntityFrameworkStores<ApplicationDbContext>().AddDefaultTokenProviders();
 
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 builder.Services.AddScoped<IDateService, DateService>();
