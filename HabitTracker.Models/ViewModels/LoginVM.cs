e1340bd49a38a61dcacf3b36d15b10d857f1e7da
Ania Bieniecka <annabieniecka5@gmail.com>
2024-07-04 13:40:29 +0200

    Created account cotroller with views (Login, Register, ForgotPassword,AccessDenied

---
+++ HabitTracker.Models/ViewModels/LoginVM.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System.ComponentModel.DataAnnotations;
+using System.Data.SqlTypes;
+using System.Reflection.Metadata.Ecma335;
+
+namespace HabitTracker.Models.ViewModels
+{
+    public class LoginVM
+    {
+        [Required]
+        [Display(Name = "User name")]
+        public string UserName { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+        [Display(Name = "Remember me")]
+        public bool RememberMe { get; set; }
+        public string? RedirectUrl { get; set; }
+    }
+}
---
+++ HabitTrackerWeb/Controllers/AccountController.cs
@@ -0,0 +1,153 @@
+﻿using Azure.Identity;
+using HabitTracker.DataAccess.Repository.IRepository;
+using HabitTracker.Models.ViewModels;
+using HabitTrackerWeb.Utility;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using System.Security.Claims;
+
+namespace HabitTrackerWeb.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly SignInManager<IdentityUser> _signInManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly IUnitOfWork _unitOfWork;
+
+        public AccountController(UserManager<IdentityUser> userManager,
+            SignInManager<IdentityUser> signInManager,
+            RoleManager<IdentityRole> roleManager, IUnitOfWork unitOfWork)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _roleManager = roleManager;
+            _unitOfWork = unitOfWork;
+        }
+        public IActionResult Login(string returnUrl = null)
+        {
+            returnUrl ??= Url.Content("~/");
+            LoginVM loginVM = new()
+            {
+                RedirectUrl = returnUrl
+            };
+
+            return View(loginVM);
+        }
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginVM loginVM)
+        {
+            if (ModelState.IsValid)
+            {
+                var result = await _signInManager.PasswordSignInAsync(loginVM.UserName, loginVM.Password, loginVM.RememberMe, false);
+                if (result.Succeeded)
+                {
+                    var user = await _userManager.FindByNameAsync(loginVM.UserName);
+                    if (await _userManager.IsInRoleAsync(user, SD.Role_Admin))
+                    {
+                        return RedirectToAction("Index", "Dashboard");
+                    }
+                    else
+                    {
+                        if (string.IsNullOrEmpty(loginVM.RedirectUrl))
+                        {
+                            return RedirectToAction("Index", "Home");
+                        }
+                        else
+                        {
+                            return LocalRedirect(loginVM.RedirectUrl);
+                        }
+                    }
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Invalid login attempt.");
+                }
+            }
+            return View(loginVM);
+        }
+        public IActionResult Register(string returnUrl = null)
+        {
+            returnUrl ??= Url.Content("~/");
+
+            if (!_roleManager.RoleExistsAsync(SD.Role_Admin).GetAwaiter().GetResult())
+            {
+                _roleManager.CreateAsync(new IdentityRole(SD.Role_Admin)).Wait();
+                _roleManager.CreateAsync(new IdentityRole(SD.Role_Customer)).Wait();
+            }
+
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterVM registerVM)
+        {
+            var users = await _userManager.Users.ToListAsync();
+
+            var userFromDb = users.FirstOrDefault(u=>u.NormalizedEmail==registerVM.UserName.ToUpper());
+
+            if(userFromDb != null)
+            {
+              ModelState.AddModelError("UserName", "User with this name already exist");
+            }
+
+            if (ModelState.IsValid)
+            {
+                IdentityUser user = new()
+                {
+                    UserName = registerVM.UserName,
+                    NormalizedUserName = registerVM.UserName.ToUpper(),
+                    Email = registerVM.Email,
+                    NormalizedEmail = registerVM.Email.ToUpper(),
+                    EmailConfirmed = true
+                };
+
+                var result = await _userManager.CreateAsync(user, registerVM.Password);
+
+                if (result.Succeeded)
+                {
+
+                    await _userManager.AddToRoleAsync(user, SD.Role_Customer);
+                    
+
+                    await _signInManager.SignInAsync(user, isPersistent: false);
+
+                    if (string.IsNullOrEmpty(registerVM.RedirectUrl))
+                    {
+                        return RedirectToAction("Index", "Home");
+                    }
+                    else
+                    {
+                        return LocalRedirect(registerVM.RedirectUrl);
+                    }
+                }
+
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError("", error.Description);
+                }
+
+            }
+
+            return View(registerVM);
+        }
+
+        public async Task<IActionResult> Logout()
+        {
+            await _signInManager.SignOutAsync();
+            return RedirectToAction("Index", "Home");
+        }
+
+        public IActionResult AccessDenied()
+        {
+            return View();
+        }
+
+        public IActionResult ForgotPassword()
+        {
+            return View();
+        }
+    }
+}
